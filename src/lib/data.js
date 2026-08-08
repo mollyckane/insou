6720daf7b84815398577c93d4b4209a1494e08fa
mollyckane <214532456+mollyckane@users.noreply.github.com>
2026-08-08 13:17:46 +0100

    create deck data
    array consisting of deck card data

---
+++ src/lib/data.js
@@ -0,0 +1,22 @@
+export const decks = [
+    {
+        id: "decks-animal-cards",
+        title: "Animal cards",
+        description: "A deck of cute animals to give you a little boost when you're feeling overwhelmed.",
+    },
+    {
+        id: "decks-affirmation-cards",
+        title: "Affirmations",
+        description: "A deck of affirmations and gentle reminders for different kinds of days.",
+    },
+    {
+        id: "decks-tiny-next-step-cards",
+        title: "Tiny next steps",
+        description: "A deck of tiny next steps to help you break down overwhelming tasks into manageable actions.",
+    },
+    {
+        id: "decks-quote-cards",
+        title: "Quotes",
+        description: "A deck of quotes to inspire and motivate you on your journey.",
+    }
+];
