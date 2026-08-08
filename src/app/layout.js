fda0b394143465e7f25c1e6d16ee1abe0e905bb9
mollyckane <214532456+mollyckane@users.noreply.github.com>
2026-08-08 13:17:18 +0100

    import navigation
    import nav for universal layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Navigation from "@/components/layout/Navigation";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -22,7 +24,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navigation />
+        {children}</body>
     </html>
   );
 }
