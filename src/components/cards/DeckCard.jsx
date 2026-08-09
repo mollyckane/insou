4b5a7eaad4bf3eb1c1037bcb8311a8eb77883959
mollyckane <214532456+mollyckane@users.noreply.github.com>
2026-08-09 11:59:08 +0100

    add size props to images

--- src/components/cards/DeckCard.jsx
+++ src/components/cards/DeckCard.jsx
@@ -41,6 +41,7 @@ export default function DeckCard({
                         src={card.image}
                         alt={card.caption}
                         fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         className="object-cover"
                     />
                 </div>
